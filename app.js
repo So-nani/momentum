d756cde589f6ec97bf2d1be6ce2656672b4b97b9
So-nani <sonani3136@gmail.com>
2025-07-03 23:13:56 +0900

    asdasd

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 // File: momentum/app.js
 const loginInput = document.querySelector('#login_form input');
-const loginButton = document.querySelector('#login_form button');
+const loginForm = document.querySelector('#login_form');
+const greeting = document.querySelector('#greeting');
 
-function LoginBtnClick(){
-  console.log(loginInput.value);
+ const HIDDEN_CLASSNAME = "hidden";
+ const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+  event.preventDefault(); // 폼 진행 방지
+  loginForm.classList.add(HIDDEN_CLASSNAME); // form 숨기기
+  const username = loginInput.value; // input의 value를 가져옴
+  localStorage.setItem(USERNAME_KEY, username); 
+  // 로컬스토리지에 username 저장
+  // console.log(loginInput.value);
+  // console.log(username);
+  paintGreetings(username);
 }
 
-loginButton.addEventListener("click",LoginBtnClick);
+function paintGreetings(abc) {
+  greeting.innerText = `Hello ${abc}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);  
+} else {
+  // show the greeting
+  paintGreetings(savedUsername);
+}
+
+// loginForm.addEventListener("submit", onLoginSubmit);
+// link.addEventListener("click", handleLinkClick);
