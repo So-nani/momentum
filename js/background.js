f66463ccdd00994317dbbe0188f4287538fc62aa
So-nani <sonani3136@gmail.com>
2025-07-08 23:11:40 +0900

    13

--- js/background.js
+++ js/background.js
@@ -9,7 +9,7 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 // html에 background-img를 적용하려면, createElement을 사용하여 img 태그를 생성하고, 
 // src 속성에 선택된 이미지를 할당합니다.
-// 그런 다음, body에 appendChild를 사용하여 추가합니다.
+// 그런 다음, body에 prepend를 사용하여 추가합니다.
 const bgimg = document.createElement("img");
 bgimg.src = `img/${chosenImage}`;
 document.body.prepend(bgimg);
